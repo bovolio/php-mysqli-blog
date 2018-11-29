9a39e4eb85ab03b715218ef41e5e9759e07084bc
Julianmaciel <julianmaciel96@gmail.com>
2018-11-29 14:02:27 -0600

    register finished; login touched; navbar fixed;

--- inc/navbar.php
+++ inc/navbar.php
@@ -10,7 +10,7 @@ if(isset($_POST['submit-search'])){
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="<?php echo ROOT_URL.'index.php'; ?>">PHP Blog</a>
+  <a class="navbar-brand" style='color:#D40ED4' href="<?php echo ROOT_URL.'index.php'; ?>">PHP Blog</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -24,9 +24,12 @@ if(isset($_POST['submit-search'])){
         <a class="nav-link" href="<?php echo ROOT_URL;?>addpost.php">Add Post<span class="sr-only">(current)</span></a>
       </li>
       </ul>
-      <ul>
-      <li style='float: right;' class="nav-item active">
-        <a class="nav-link" href="<?php echo ROOT_URL.'register.php'; ?>">Register<span class="sr-only">(current)</span></a>
+      <ul style='list-style-type: none;'>
+      <li style='float: right;' class="nav-item active" style='list-style-type: none;'>
+        <a class="nav-link" style='color:#fff' href="<?php echo ROOT_URL.'register.php'; ?>">Register<span class="sr-only">(current)</span></a>
+      </li>
+      <li style='float: right;' class="nav-item active" style='list-style-type: none;'>
+        <a class="nav-link" style='color:#fff' href="<?php echo ROOT_URL.'login.php'; ?>">Log in<span class="sr-only">(current)</span></a>
       </li>
     </ul>
 
--- register.php
+++ register.php
@@ -7,70 +7,98 @@
     if(isset($_POST['submit'])){
     // Form Variables
     $username= mysqli_real_escape_string($conn, $_POST['username']);
-    $password= mysqli_real_escape_string($conn, $_POST['password']);
     $email= mysqli_real_escape_string($conn, $_POST['email']);
 
         //Check to see values are entered in forms & DB TABLE
-
         if (!isset($_POST['username'] , $_POST['password'], $_POST['email'])) {
             die ('Please complete the registration form!<br><a href="register.php">Back</a>');
         }
-
         if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
             die ('Please complete the registration form!<br><a href="register.php">Back</a>');
         }
+
+        if(empty(trim($_POST["password"]))){
+            $password_err = "Please enter a password.";     
+        } elseif(strlen(trim($_POST["password"])) < 6){
+            $password_err = "Password must have atleast 6 characters.";
+        } else{
+            $password = trim(mysqli_real_escape_string($conn, $_POST['password']));
+        }
+
+        if(empty(trim($_POST["confirm_password"]))){
+            $confirm_password_err = "Please confirm password.";     
+        } else{
+            $confirm_password = trim(mysqli_real_escape_string($conn, $_POST["confirm_password"]));
+            if(empty($password_err) && ($password != $confirm_password)){
+                $confirm_password_err = "Password did not match.";
+        }
+    } 
+        if ($stmt = $conn->prepare('SELECT id, password FROM accounts WHERE email = ?')){
+            $stmt->bind_param('s', $_POST['email']);
+            $stmt->execute(); 
+            $stmt->store_result(); 
+            // Store the result so we can check if the email exists in the database.
+            if ($stmt->num_rows > 0) {
+                // Username already exists
+                $email_err = "E-mail already exists! Please choose another.";
+        } else {
         if ($stmt = $conn->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
-            // Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
             $stmt->bind_param('s', $_POST['username']);
             $stmt->execute(); 
             $stmt->store_result(); 
             // Store the result so we can check if the account exists in the database.
             if ($stmt->num_rows > 0) {
                 // Username already exists
-                echo "<div class='alert alert-dismissible alert-danger' name='alert-danger'>
-                <button type='button' class='close' data-dismiss='alert'>&times;</button>
-                <strong>Username Exists!</strong> Please Choose another one.
-              </div>";
+                $username_err= "Username already exists! Please choose another.";
             } else {
-                // Username doesnt exists, insert new account
+                //if errors are empty
+                if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
+                // Username and Email don't exist, insert new account
                 if ($stmt = $conn->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
-                    // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
                     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
                     $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
                     $stmt->execute();
-                    echo "<div class='alert alert-dismissible alert-success' name='alert-success'>
+                    $alert = "<div class='alert alert-dismissible alert-success' name='alert-success'>
                     <button type='button' class='close' data-dismiss='alert'>&times;</button>
                     <strong>Well done!</strong> Account Created Successfully!
                   </div>";
-                } else {
-                    echo 'Could not prepare statement!';
+                } 
                 }
             }
             $stmt->close();
-        } else {
-            echo 'Could not prepare statement!';
         }
         $conn->close();
     }
+}
+}
 ?>
 
 <?php include 'inc/header.php'; ?>
 <?php include 'inc/navbar.php'; ?>
+<?php echo $alert;?>
 <div class='container'>
     <br>
     <h1>Register</h1>
     <form method='POST' action='<?php $_SERVER['PHP_SELF'];?>'>
         <div class='form-group'>
             <label>Username</label>
-            <input type='text' name='username' place-holder='Username' class='form-control' required>
+            <input type='text' name='username' class='form-control' value="<?php echo $username; ?>" required>
+            <span class="help-block"style='color:red;'><?php echo $username_err; ?></span>
         </div>
         <div class='form-group'>
             <label>Password</label>
-            <input type='passsword' name='password' place-holder='Password' class='form-control' required>
+            <input type='password' name='password' class='form-control' required>
+            <span class="help-block"style='color:red;'><?php echo $password_err; ?></span>
         </div>
         <div class='form-group'>
+        <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control" required>
+                <span class="help-block"style='color:red;'><?php echo $confirm_password_err; ?></span>
+            </div>
+        <div class='form-group'>
             <label>E-mail</label>
-            <input type='email' name='email' place-holder='E-mail' class='form-control' required>
+            <input type='email' name='email' class='form-control' value="<?php echo $email; ?>" required>
+            <span class="help-block"style='color:red;'><?php echo $email_err; ?></span>
         </div>
         <input type='submit' value='submit' name='submit' class='btn btn-primary'>
     </form>
