219ae81a5013fff55f7eaced22956607d2d6fc7c
Julianmaciel <julianmaciel96@gmail.com>
2018-11-30 10:33:13 -0600

    contributor constants added;

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 
-    define('ROOT_URL', 'localhost:3000/');
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', 'root');
-    define('DB_NAME', 'testdb');
+    define('ROOT_URL', 'YOUR_ROOT_URL');
+    define('DB_HOST', 'YOUR_DB_HOST');
+    define('DB_USER', 'YOUR_DB_USER');
+    define('DB_PASS', 'YOUR_DB_PASS');
+    define('DB_NAME', 'YOUR_DB_NAME');
 
 ?>
