702d6366d17d087456389f0ef39cdfba90617185
Julianmaciel <julianmaciel96@gmail.com>
2018-11-30 10:40:48 -0600

    remove accidental check in

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    define('ROOT_URL', 'YOUR_ROOT_URL');
-    define('DB_HOST', 'YOUR_DB_HOST');
-    define('DB_USER', 'YOUR_DB_USER');
-    define('DB_PASS', 'YOUR_DB_PASS');
-    define('DB_NAME', 'YOUR_DB_NAME');
-
-?>
